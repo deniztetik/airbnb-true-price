01445a6077da69ddaa4842171d87582567ca9459
Deniz Tetik <deniztetik31@gmail.com>
2022-04-12 22:53:46 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+//@ts-check
+
+/**
+ * INSTRUCTIONS: for now, copy and paste in an Airbnb listings result view
+ */
+
+let previousPrice = '';
+let previousUrl = '';
+
+// const observer = new MutationObserver(function () {
+//     const firstSpan = document.querySelector('[style^="--pricing"] span[aria-hidden]');
+//     const newPrice = firstSpan.textContent;
+
+//     const changeConditions = [
+//         newPrice !== previousPrice,
+//         // location.href !== previousUrl,
+//     ];
+
+//     if (changeConditions.some((condition) => !!condition)) {
+//         console.log(`URL changed to ${location.href}`);
+//         console.log('+++prevPrice, newPrice', previousPrice, newPrice);
+//         previousPrice = newPrice;
+//         previousUrl = location.href;
+//         replacePrices();
+//     }
+// });
+// const config = { subtree: true, childList: true };
+// observer.observe(document, config);
+
+function replacePrices() {
+    const prices = [...document.querySelectorAll('[style^="--pricing"] [aria-hidden]')];
+
+    const divs = prices.filter(el => el.nodeName === "DIV");
+    const spans = prices.filter(el => el.nodeName === "SPAN");
+
+    /* const pricePerNight = parseInt(spans[0].textContent.match(/\d+/)[0]) / 3; */
+
+    /* divs[0].childNodes[1].textContent = '$' + pricePerNight; */
+
+    for (let i = 0; i < divs.length; i++) {
+        const childNodes = divs[i].childNodes;
+        const nightlyPriceNode = childNodes[childNodes.length - 2];
+
+        const totalPriceNode = spans[i];
+        const pricePerNight = Math.round(parseInt(totalPriceNode.textContent.replaceAll(',', '').match(/\d+/)[0]) / 3);
+
+        replacePrice(childNodes[childNodes.length - 2], pricePerNight);
+    }
+};
+
+function replacePrice(node, newPrice) {
+    node.textContent = '$' + newPrice
+};
+
+replacePrices();
+subscribeToListingsCardRowChanges();
+subscribeToLegacyListingSectionItemChanges();
+subscribeToItemListChanges();
+
+const cardRowTextContent = [];
+const legacyListingSectionItemTextContent = [];
+let itemListTextContent = '';
+
+function subscribeToListingsCardRowChanges() {
+    [...document.querySelectorAll('[data-testid="listings-card-row"]')].forEach((row, idx) => {
+        const observer = new MutationObserver(function () {
+            if (row.textContent !== cardRowTextContent?.[idx]) {
+                console.log('row textContent changed!');
+                cardRowTextContent[idx] = row.textContent;
+                replacePrices();
+            }
+        });
+        const config = { subtree: true, childList: true };
+        observer.observe(row, config);
+    });
+}
+
+function subscribeToLegacyListingSectionItemChanges() {
+    [...document.querySelectorAll('[data-testid="shimmer-legacy-listing-section-item"]')].forEach((row, idx) => {
+        const observer = new MutationObserver(function () {
+            if (row.textContent !== legacyListingSectionItemTextContent?.[idx]) {
+                console.log('section item textContent changed!');
+                legacyListingSectionItemTextContent[idx] = row.textContent;
+                replacePrices();
+            }
+        });
+        const config = { subtree: true, childList: true };
+        observer.observe(row, config);
+    });
+}
+
+function subscribeToItemListChanges() {
+    const itemListElement = document.querySelector('[itemprop="itemListElement"]');
+    const itemList = itemListElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    const observer = new MutationObserver(function () {
+        if (itemList.textContent !== itemListTextContent) {
+            itemListTextContent = itemList.textContent;
+            replacePrices();
+        }
+    });
+    const config = { subtree: true, childList: true };
+    observer.observe(itemList, config);
+}
